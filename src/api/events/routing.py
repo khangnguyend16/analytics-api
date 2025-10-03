c4636c44d591ae2dc35099f3b774327d5a2b3be6
Khang <khangnguyend16@gmail.com>
2025-10-03 19:00:05 +0700

    update

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,16 +1,49 @@
 from fastapi import APIRouter
-from .schemas import EventSchema, EventListSchema
+from .schemas import EventSchema, EventListSchema, EventCreateSchema, EventUpdateSchema
 
 router = APIRouter()
 
 
+# Get data here
+# List view
+# GET /api/events
 @router.get("/")
 def read_events() -> EventListSchema:
     # a bunch of items in a table
     return {"results": [{"id": 1}, {"id": 2}, {"id": 3}], "count": 3}
 
 
+# Send data here
+# create view
+# POST /api/events/
+@router.post("/")
+def create_event(payload: EventCreateSchema) -> EventSchema:
+    # a bunch of items in a table
+    print(payload.page)
+    data = payload.model_dump()  # payload -> dict (pydantic)
+    return {"id": 123, **data}  # unpack data
+
+
+# GET /api/events/12
 @router.get("/{event_id}")
-def get_events(event_id: int) -> EventSchema:
+def get_event(event_id: int) -> EventSchema:
     # a single row
     return {"id": event_id}
+
+
+# Update this data
+# PUT /api/events/12
+@router.put("/{event_id}")
+def update_event(event_id: int, payload: EventUpdateSchema) -> EventSchema:
+    # a single row
+    print(payload.description)
+    data = payload.model_dump()  # payload -> dict (pydantic)
+    return {"id": event_id, **data}  # # unpack data
+
+
+# # Delete this data
+# # PUT /api/events/12
+# @router.put("/{event_id}")
+# def update_event(event_id: int, payload: dict = {}) -> EventSchema:
+#     # a single row
+#     return {"id": event_id}
--- src/api/events/schemas.py
+++ src/api/events/schemas.py
@@ -1,9 +1,26 @@
-from pydantic import BaseModel
-from typing import List
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+"""
+id
+path
+description
+"""
+
+
+class EventCreateSchema(BaseModel):
+    page: str
+    description: Optional[str] = Field(default="")
+
+
+class EventUpdateSchema(BaseModel):
+    description: str
 
 
 class EventSchema(BaseModel):
     id: int
+    page: Optional[str] = ""
+    description: Optional[str] = ""
 
 
 class EventListSchema(BaseModel):
