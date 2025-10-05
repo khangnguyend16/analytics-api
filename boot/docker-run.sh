6ee16f0178b057fbf4c40b9ccf4871fcb53891e2
Khang <khangnguyend16@gmail.com>
2025-10-05 23:20:38 +0700

    update

--- boot/docker-run.sh
+++ boot/docker-run.sh
@@ -4,6 +4,6 @@ source /opt/venv/bin/activate
 
 cd /code
 RUN_PORT=${PORT:-8000}
-RUN_HOST=${HOST:-0.0.0.0}
+RUN_HOST=${HOST:-0.0.0.0} # HOST=[::]  # ipv6
 
 gunicorn -k uvicorn.workers.UvicornWorker -b $RUN_HOST:$RUN_PORT main:app 
